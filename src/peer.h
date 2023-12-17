c077c3e50fff44cdbc61b59e4524487887e5605c
Khthonian <anansicorp@tuta.io>
2023-12-17 16:13:40 +0000

    feat(src): Add Windows socket functioning
    The necessary code had been added for this application to also function
    on Windows however it is untested. Primarily, it exists to show
    consideration.

--- src/peer.h
+++ src/peer.h
@@ -1,9 +1,18 @@
 // Copyright 2023 Stewart Charles Fisher II
 
 // Include libraries
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <arpa/inet.h>
 #include <netinet/in.h>
-#include <opencv2/core/hal/interface.h>
 #include <sys/socket.h>
+#include <unistd.h>
+#endif  // _WIN32
+
+#include <opencv2/core/hal/interface.h>
 
 #include <algorithm>
 #include <cstddef>
