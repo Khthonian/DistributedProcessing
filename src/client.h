e731f68327b75380484d49350a9088660a8238b8
Khthonian <anansicorp@tuta.io>
2023-12-17 01:29:29 +0000

    refactor(src): Reduce code bloat
    The code in the instruction source files didn't actually need to be
    abstracted to a separate file because certain parts were unique to the
    server and others were unique to the client. As such, the respective
    parts have been collapsed into their respective classes to reduce
    unnecessary file bloat.

---
+++ src/client.h
@@ -0,0 +1,64 @@
+// Copyright 2023 Stewart Charles Fisher II
+
+// Import libraries
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <opencv2/core/hal/interface.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "transmission.h"
+
+#ifndef SRC_CLIENT_H_
+#define SRC_CLIENT_H_
+
+class Client {
+ private:
+  // Define an enumeration for parameter types
+  enum class ParamType {
+    Double,
+    Integer,
+    String,
+  };
+
+  // Define s struct for the filter requirements
+  struct FilterRequirement {
+    ParamType paramType;
+    std::string expectedValue;
+  };
+
+  // Define a map to hold the requirements for each filter
+  std::unordered_map<std::string, FilterRequirement> filterRequirements = {
+      {"resize", {ParamType::Double, ""}},
+      {"rotate", {ParamType::Double, ""}},
+      {"flip", {ParamType::Integer, ""}},
+      {"brightness", {ParamType::Double, ""}},
+      {"contrast", {ParamType::Double, ""}},
+      {"gamma", {ParamType::Double, ""}},
+      {"colour", {ParamType::String, "rgb|hsv|grey|ycc|hsl"}},
+      {"smooth", {ParamType::String, "gauss|box|sharp"}},
+  };
+
+  // Define a function to validate the user input
+  bool validateFilterInput(const std::string& operation,
+                           const std::string& param);
+
+ public:
+  // Define a function to manage client operation
+  void operateClient(const std::string serverAddress,
+                     const std::string imagePath, const std::string operation,
+                     const std::string param);
+};
+
+#endif  // SRC_CLIENT_H_
